f33281a6819a2e450446e9de930298d8fc332fef
aggabaorenz16 <43602006+aggabaorenz16@users.noreply.github.com>
2021-08-19 00:17:23 +1300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+ /* Axios */
+const fetch = () => {
+let result = axios({
+     /* URL */
+        url: 'https://coronavirus-19-api.herokuapp.com/countries/Philippines',
+         /* Method GET */
+        method: 'GET',
+         /* Success */
+    }).then((Response) => {
+        return Response
+         /* Rejected */
+    }).catch((Reject) => {
+        return Reject
+    })
+     /* Result */
+    return result
+}
+fetch().then((Response) => {
+     /* Today Cases */
+    document.querySelector("#case").innerHTML = Response.data.todayCases;
+     /* Recovered */
+    document.querySelector("#recovered").innerHTML = Response.data.recovered;
+     /* Today Deaths */
+    document.querySelector("#tdeaths").innerHTML = Response.data.todayDeaths;
+     /* Deaths */
+    document.querySelector("#deaths").innerHTML = Response.data.deaths;
+     /* Active */
+    document.querySelector("#active").innerHTML = Response.data.active;
+     /* Critical */
+    document.querySelector("#critical").innerHTML = Response.data.critical;
+    /* Total Test */
+    document.querySelector("#Ttest").innerHTML = Response.data.totalTests;
+})
+/* Disable Inspect Element */
+/* document.onkeydown = function(e) {
+    if(event.keyCode == 123) {
+       return false;
+    }
+    Ctrl+Shift+I
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+       return false;
+    }
+    Ctrl+Shift+C
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+       return false;
+    }
+    Ctrl+Shift+J
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+       return false;
+    }
+    Ctrl+Shift+U
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+       return false;
+    }
+  } */
